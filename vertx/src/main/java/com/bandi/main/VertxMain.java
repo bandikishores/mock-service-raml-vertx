2b73cc0812e4b2a52a195659e9372fb6311ff7a3
Kishore <kishore.bandi@inmobi.com>
2016-02-02 18:44:35 +0530

    Modifying the classpath

--- vertx/src/main/java/com/bandi/main/VertxMain.java
+++ vertx/src/main/java/com/bandi/main/VertxMain.java
@@ -4,10 +4,14 @@ import com.bandi.verticle.MyVerticle;
 import io.vertx.core.Vertx;
 
 public class VertxMain {
-
+	
 	public static void main(String[] args) {
 		 Vertx vertx = Vertx.vertx();
 	     vertx.deployVerticle(new MyVerticle());
+	     
+	     vertx.deployVerticle(new MyVerticle(), stringAsyncResult -> {
+	         System.out.println("BasicVerticle deployment complete");
+	     });
 	}
 
 }
--- vertx/src/main/java/com/bandi/verticle/MyVerticle.java
+++ vertx/src/main/java/com/bandi/verticle/MyVerticle.java
@@ -7,7 +7,7 @@ public class MyVerticle extends AbstractVerticle {
 	@Override
     public void start(Future<Void> startFuture) { 
         System.out.println("MyVerticle started!");
-    }
+    } 
 
     @Override
     public void stop(Future stopFuture) throws Exception {
